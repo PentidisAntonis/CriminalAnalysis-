332e0a771d438238e7fc70768dde0aac5862f4e6
ahpen <a.h.pentidis@gmail.com>
2023-11-29 19:47:23 +0200

    the new workbook has been created and is almost ready to use

--- venv/CriminalMain.py
+++ venv/CriminalMain.py
@@ -5,10 +5,9 @@ xlxs_file_path = r'C:\Users\ahpen\PycharmProjects\pythonProject\venv\Mother Jone
                  r'- Mass Shootings Database, 1982 - 2019.xlsx'
                         #Setting the working directory
 
-workbook=pp.load_workbook(xlxs_file_path)   #Loading our file
-sheet = workbook.active                     #It is only one sheet but to be safe i call the active
-df=pd.DataFrame(sheet.values)               #Creating a Pandas DataFrame from the sheet above
-print("Original DataFrame: ", df)           #I want to see how it will print it, it was awful
+
+df=pd.read_excel(xlxs_file_path, header=0)  #Read the Excel file with the first row as the header
+#print("Original DataFrame: ", df)           #I want to see how it will print it, it was awful
 
 #for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row, min_col=1, max_col=sheet.max_column):
  #   for cell in row:
@@ -17,14 +16,25 @@ print("Original DataFrame: ", df)           #I want to see how it will print it,
 
 new_workbook = pp.Workbook()   #Creating a new workbook so that i can drop some columns there are 26 in total
 new_sheet = new_workbook.active
+
+
+columns_to_drop = ["summary", "mental_health_details", "weapons_obtained_legally",
+                   "where_obtained", "weapon_type",	"weapon_details", "sources", "mental_health_sources",
+                   "sources_additional_age",	"latitude",	"longitude" ]
+df = df.drop(columns=columns_to_drop)
+
+# Write header row to the new workbook because if not we lose it
+new_sheet.append(df.columns.tolist())
+
 for index, row in df.iterrows():    #Going through rows in the modified DataFrame and append them to the new sheet
     new_sheet.append(row.tolist())
 
 new_workbook.save('modified_file.xlsx')
 
-header=df.columns.tolist()
-print('\t'.join(header))
+# Read the modified workbook into a new DataFrame so that i can use it
+modified_df = pd.read_excel('modified_file.xlsx', header=0)
 
-
-workbook.close()
+# Print the modified DataFrame
+print("Modified DataFrame: ", modified_df)
 new_workbook.close()
+
